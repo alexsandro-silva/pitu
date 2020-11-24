1610baec902f26b8cadeced8bc02c0b1c279272d
Alexsandro Rodrigues <alexsandrorodriguess@gmail.com>
2020-11-24 12:28:20 -0300

    Implementação da persistencia de dados em memória

--- backend/src/controllers/links.ts
+++ backend/src/controllers/links.ts
@@ -1,15 +1,47 @@
 import {Request, Response} from 'express';
+import { Link } from '../models/link';
+
+const links: Link[] = [];
+let proxId = 1;
+
+function generateCode() {
+    let text='';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let index = 0; index < 5; index++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+}
 
 function postLink(request: Request, response: Response) {
-    response.send('postLink');
+    const link = request.body as Link;
+    link.id = proxId++;
+    link.code = generateCode();
+    link.hits = 0;
+    links.push(link);
+    response.status(201).json(link);
 }
 
 function getLink(request: Request, response: Response) {
-    response.send('getLink');
+    const code = request.params.code as string;
+    const link = links.find(item => item.code === code);
+    if(!link) {
+        response.sendStatus(404);
+    } else {
+        response.json(link);
+    }
 }
 
 function hitLink(request: Request, response: Response) {
-    response.send('hitLink');
+    const code = request.params.code as string;
+    const index = links.findIndex(item => item.code === code);
+    if(index === -1) {
+        response.sendStatus(404);
+    } else {
+        links[index].hits!++;
+        response.json(links[index]);
+    }
 }
 
 export default {
